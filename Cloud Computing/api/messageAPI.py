89e3667c1372bbdcb354d8a5ac7035c0694b5f7d
TarmanaM <94690684+TarmanaM@users.noreply.github.com>
2023-06-02 20:19:16 +0700

    Update messageAPI.py
    fixing message atribut
    add rorator sales

--- Cloud Computing/api/messageAPI.py
+++ Cloud Computing/api/messageAPI.py
@@ -1,74 +1,94 @@
 from flask import Blueprint, jsonify, request
 from firebase_admin import firestore
+import uuid
 
 db = firestore.client()
+
 messageAPI = Blueprint('messageAPI', __name__)
+
+message_ref = db.collection('message')
+chat_ref = db.collection('chat')
+sales_ref = db.collection('Sales')
 
 #Tambah Data dengan tambahan field
 @messageAPI.route('/add', methods=['POST'])
 def add_message():
+    try:
+    #get pesan wa
+        data = request.get_json()
+        id_message = data.get('id_message')
+        message_text = data.get('message_text')
+        sender_id = data.get('sender_id')
+        timestamp = data.get('time_stamp')
 
-#get pesan wa
-
-    data = request.get_json()
-    message_ref = db.collection('message')
-
-    id_message = data.get('id_message')
-    message_text = data.get('message_text')
-    senderID = data.get('senderID')
-    timestamp = data.get('time_stamp')
-    
-    #Peanambahan field yang diperlukan
-    data['read_status']=False
-    data['sentiment_status']=False
-    data['positive']=0
-    data['negative']=0
-    
-    #Bagian kesiapa 
-    data['chat_id'] = 1
-    data['sales'] = 0
-
- 
-    message_ref.document().set(data)
-
-    return jsonify({'message': 'Message added successfully'}), 200
-
-#Tampilkan chat terbaru tiap chat client 
-#PROMBLEM 1
-# @messageAPI.route('/new/<chat_id>', methods=['GET'])
-# def get_new_messages_by_chat_id(chat_id):
-#     messages_ref = db.collection('message')
-#     query = messages_ref.where('chat_id', '==', chat_id).order_by('timestamp', direction=firestore.Query.DESCENDING).limit(1)
-#     messages = query.get()
-
-#     all_messages = []
-#     for message in messages:
-#         message_data = message.to_dict()
-#         all_messages.append(message_data)
-
-#     return jsonify(all_messages), 200
+        #Peanambahan field yang diperlukan
+        data['read_status']=False
+        #FUNGSI SENTIMENT ML NYA 
 
 
+        # funngsi sentiment_analyis(data.get(message_text))
+        data['positive']=0
+        data['negative']=0
+        query = message_ref.where('sender_id', '==',sender_id).get()
+        if not query:
+            #ROTATOR SALES
+            sales_query = sales_ref.order_by('client_handler').limit(1).get()
+            if sales_query:
+                sales_id = sales_query[0].id
+                client_handler = sales_query[0].get('client_handler')
+                # Increment client_handler by 1
+                client_handler += 1
+                # Update the document with the new client_handler value
+                sales_ref.document(sales_id).update({'client_handler': client_handler})
+                # Retrieve the data from the request
+                # Add sales_id to the data
+                data['sales_id'] = sales_id   
+            else :
+                data['sales_id'] = 'sales-12345678'
+            #Buat chat baru  
+            random_number = str(uuid.uuid4())[:15]
+            chat_id = "chat-"+random_number
+            if not chat_ref.document(chat_id).get().exists:            
+                chat_data = {
+                    'alamat':'-',
+                    'catatan':'-',
+                    'chat_id': chat_id,
+                    'kota':'-',
+                    'name':'-',
+                    'produk':'-',
+                    'revenue':0,
+                    'sender_id':sender_id,
+                    'sales_id': sales_id,
+                    'status': 'baru'
+                }
+                data['chat_id']=chat_id
+                chat_ref.document(chat_id).set(chat_data)
+            message_ref.document().set(data)
+            data['sender_id']= sender_id
+            return jsonify({'message': 'Message & Chat added successfully'},data), 200
+        data['sender_id']= sender_id
+        message_ref.document().set(data)
+        return jsonify({'message': 'Message added successfully'},data), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500 
 
 #Tampilkan message secara Kondisi setiap chat
-@messageAPI.route('/all/<int:chat_id>', methods=['GET'])
-def get_messages_by_chat_id(chat_id):
-    messages_ref = db.collection('message')
-    query = messages_ref.where('chat_id', '==', chat_id).get()
-
-    all_messages = []
-    for message in query:
-        message_data = message.to_dict()
-        all_messages.append(message_data)
-
-    return jsonify(all_messages), 200
-
+@messageAPI.route('/all/<string:chat_id>', methods=['GET'])
+def get_messages_by_chat_ids(chat_id):
+    try:
+        message_ref = db.collection('message')
+        query = message_ref.where('chat_id', '==', chat_id).get()
+        all_messages = []
+        for message in query:
+            message_data = message.to_dict()
+            all_messages.append(message_data)
+        return jsonify(all_messages), 200
+    except Exception as e:
+        return jsonify({'error': str(e)}), 500 
 #Tampilkan seluruh message pada data base
 @messageAPI.route('/all', methods=['GET'])
 def get_all_messages():
-    messages_ref = db.collection('message')
-    messages = messages_ref.get()
-
+    messages = message_ref.get()
     all_messages = []
     for message in messages:
         message_data = message.to_dict()
