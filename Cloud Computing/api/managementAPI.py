4d401713dd1f69bc57c20ee511a8b3a8af237e80
TarmanaM <94690684+TarmanaM@users.noreply.github.com>
2023-06-07 11:30:39 +0700

    Update managementAPI.py
    Final Endpoint Management v1.0

--- Cloud Computing/api/managementAPI.py
+++ Cloud Computing/api/managementAPI.py
@@ -3,21 +3,66 @@ from firebase_admin import firestore
 
 db = firestore.client()
 managementAPI = Blueprint('managementAPI', __name__)
-sales_ref = db.collection('sales')
-chat_ref = db.collection('chat')    
-def id_checker(sales_id,chat_id):
-    sales_id_query = sales_ref.where('sales_id', '==', sales_id).get()
-    chat_id_query = chat_ref.where('chat_id', '==', chat_id).get()
-    if not sales_id_query:
-        raise ValueError(f'Sales account id :{sales_id} tidak ditemukan')
-    if not chat_id_query:
-        raise ValueError(f'Chat id : {chat_id} tidak ditemukan')
-    
 
+
+from .function import id_checker_sales,id_checker_chat, sales_ref, chat_ref, message_ref ,id_checker_message,generate_rdm_id, db
+
+    
+@managementAPI.route('/list/<string:sales_id>',methods=['GET'])
+def list_management(sales_id):
+    try:
+        id_checker_sales(sales_id)
+        
+        #Query untuk mengambil data chat yang berkondisi terakhir chat paing baru
+        chat_query = chat_ref.where('sales_id', '==', sales_id).order_by('last_timestamp', direction=firestore.Query.DESCENDING).get()
+
+        management = []
+        
+        for chat in chat_query:
+            chat_data = chat.to_dict()
+            chat_id = chat.id
+            name = chat_data.get('name')
+            status = chat_data.get('status')
+            produk = chat_data.get('produk')
+            sender_id = chat_data.get('sender_id')
+            revenue = chat_data.get('revenue')
+            mulai_chat=chat_data.get('first_timestamp')
+            terakhir_chat=chat_data.get('last_timestamp')
+            alamat = chat_data.get('alamat')
+            kota = chat_data.get('kota')
+            catatan = chat_data.get('catatan')
+            new_message = chat_data.get('new_message_count')
+
+
+            #wadah untuk menambahkan data chat sehingga dapat diurutkan
+            management.append({
+                    'chat_id': chat_id,
+                    'name': name,
+                    'status': status,
+                    'produk':produk,
+                    'revenue':revenue,
+                    'mulai_chat':mulai_chat,
+                    'terakhir_chat':terakhir_chat,
+                    'alamat':alamat,
+                    'kota':kota,
+                    'catatan':catatan,
+                    'new_message':new_message                    
+                })
+
+        return jsonify({'message':'connected'},management), 200
+    except Exception as e:
+        return jsonify({'message': 'Access Denied', 'error': str(e)}), 401
+
+
+
+
+
+#menyimpan perubaahan pada Edit Management
 @managementAPI.route('/save/<string:sales_id>/<string:chat_id>', methods=['PUT'])
 def save_chat(sales_id,chat_id):
     try:
-        id_checker(sales_id,chat_id)
+        id_checker_sales(sales_id)
+        id_checker_chat(chat_id)
         chat_doc= chat_ref.document(chat_id)
 
         data = request.get_json()
@@ -43,12 +88,17 @@ def save_chat(sales_id,chat_id):
     except Exception as e:
         return jsonify({'message': 'Access Denied', 'error': str(e)}), 401
 
+#mengambil data yang lama pada edit chat
 @managementAPI.route('/edit/<string:sales_id>/<string:chat_id>', methods=['GET'])
 def edit_chat(sales_id,chat_id):
     try:
+        
+        id_checker_sales(sales_id)
+        id_checker_chat(chat_id)
+
         query = chat_ref.where('chat_id', '==', chat_id).get()
-        id_checker(sales_id,chat_id)
         chat_data = query[0].to_dict()
+
         status = chat_data['status']
         name = chat_data['name']
         # nomor_hp = chat_data['nomor_hp']
