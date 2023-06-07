bf452678b351fdf8c88188233bd2029e344ce95b
TarmanaM <94690684+TarmanaM@users.noreply.github.com>
2023-06-07 11:34:53 +0700

    Update sentimentResultAPI.py
    Final sentiment function v1.0

--- Cloud Computing/api/sentimentResultAPI.py
+++ Cloud Computing/api/sentimentResultAPI.py
@@ -4,8 +4,6 @@ from tensorflow.keras.models import load_model
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 
-sentimentResultAPI = Blueprint('sentimentResultAPI', __name__) #Membuat blueprint untuk mendefinisikan API sentimentResultAPI
-app = Flask(__name__) #Membuat instance untuk menjalankan server
 model = load_model('api/sentiment_model_qustomate_v4.h5') #Memuat model yang telah dilatih
 tokenizer = Tokenizer() #Digunakan untuk pre-processing teks
 
@@ -23,21 +21,9 @@ def predict_sentiment(text):
     # Mengembalikan hasil prediksi
     threshold = 0.6
     if prediction < threshold:
-        sentiment = 0, 'negative'
+        sentiment = 0
     else:
-        sentiment = 1, 'positive'
+        sentiment = 1
     
     return sentiment
 
-# Endpoint API untuk prediksi sentimen
-@sentimentResultAPI.route('/predict_sentiment', methods=['POST'])
-def predict_sentiment_api():
-    # data = request.json()  # Menerima data teks dari klien
-    # text = data['text']
-    text = request.json['text']
-    # Melakukan prediksi sentimen
-    sentiment = predict_sentiment(text)
-
-    # Mengembalikan hasil prediksi sebagai respons JSON
-    response = {'sentiment': sentiment}
-    return jsonify({'message': 'Melakukan predict', 'sentiment': sentiment})
